16980f8051910d34847c4ec3e91f8e31e0f9e1e6
Andres Fernandez <ajfernandez.roa@gmail.com>
2026-05-17 19:39:32 -0400

    WIP: desingConfigurator and design page

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,33 @@
-const Page = async () => {};
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
+const Page = async ({ searchParams }: PageProps<"/configure/design">) => {
+  const { id } = await searchParams;
+
+  if (!id || typeof id != "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id: id.trim() },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={id}
+      imageUrl={imageUrl}
+      imageDimensions={{ width, height }}
+    />
+  );
+
+  return <p>{id}</p>;
+};
 
 export default Page;
